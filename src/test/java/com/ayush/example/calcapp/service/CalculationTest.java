09f75b84dbb701bb3f229571b1754817a07bc989
AyushMhetre12 <ayush.mhetre@oracle.com>
2026-07-29 15:28:42 +0530

    hi

--- src/test/java/com/ayush/example/calcapp/service/CalculationTest.java
+++ src/test/java/com/ayush/example/calcapp/service/CalculationTest.java
@@ -41,7 +41,7 @@ public class CalculationTest {
     }
     @Test
     public void testDiv(){
-
+        Assertions.assertNotNull(calculation,"object is null");
         Assertions.assertEquals(12,(int)calculation.div(48,4));
 
 
